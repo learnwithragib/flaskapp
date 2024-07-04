fdab6f85a1bd8ce16179cbd907b5e12cbdce060a
learnwithragib <146671689+learnwithragib@users.noreply.github.com>
2024-07-04 09:11:37 -0400

    Create config.py

---
+++ config.py
@@ -0,0 +1,10 @@
+# config.py
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+class Config:
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'your-secret-key'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///groqbook.db'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
